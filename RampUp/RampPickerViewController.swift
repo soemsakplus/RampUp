396d08f4c1893d415a1ede499a3c0b75e0ce1bee
Soemsak <soemsakplus@gmail.com>
2018-08-04 12:03:02 +0700

    Breeze - init project

---
+++ RampUp/RampPickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RampPickerViewController.swift
+//  RampUp
+//
+//  Created by soemsak on 4/8/2561 BE.
+//  Copyright © 2561 soemsak.com. All rights reserved.
+//
+
+import UIKit
+
+class RampPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
